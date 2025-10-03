7fd2d07bd91509ded04ddda3fdfb1f7f9d981b17
johnrandallia <eiselem9@gmail.com>
2025-10-03 13:38:29 +0100

    unicode was not rendered correctly. Changed icon and moved the button to avoid overlaps with copy button

--- docs/_static/js/downloadbutton.js
+++ docs/_static/js/downloadbutton.js
@@ -8,20 +8,25 @@ document.addEventListener("DOMContentLoaded", () => {
     button.className = "downloadbtn";
     button.title = "Download code";
 
-    // Unicode  (you can swap for an SVG icon if you want)
-    button.innerHTML = "?";
+    // Use a down-arrow SVG (cleaner than Unicode)
+    button.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" 
+           fill="currentColor" viewBox="0 0 16 16">
+        <path d="M.5 9.9a.5.5 0 0 1 .5-.5h4V1.5a.5.5 0 0 1 1 0v7.9h4a.5.5 0 0 1 .4.8l-4.5 5a.5.5 0 0 1-.8 0l-4.5-5a.5.5 0 0 1-.1-.3z"/>
+      </svg>
+    `;
 
-    // Add click handler
+    // Click handler → download content
     button.addEventListener("click", () => {
       let code = pre.innerText;
       let blob = new Blob([code], { type: "text/plain" });
       let link = document.createElement("a");
       link.href = URL.createObjectURL(blob);
-      link.download = "script.txt"; // you could vary this by language, etc.
+      link.download = "script.py"; // default
       link.click();
     });
 
-    // Insert button into the block
+    // Insert button
     block.style.position = "relative";
     block.appendChild(button);
   });
